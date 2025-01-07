8b9751b0a09b1c05a52833266c6c048994858604
eunsungtrasys <eunsungtrasys@gmail.com>
2025-01-07 11:29:35 +0900

    error handler

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -279,6 +279,6 @@ def handle_internal_server_error(e):
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=3000, debug=True)
-    # app.run('0.0.0.0', port=5000, debug=True)
+    # app.run('0.0.0.0', port=3000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
     
